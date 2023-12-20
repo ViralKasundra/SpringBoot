3833f890f04972235b4453c2d8439888b11d5879
viral Kasundra <145787515+ViralKasundra@users.noreply.github.com>
2023-12-20 10:46:43 +0530

    Add files via upload

---
+++ SpringBootOpenAPIGenerator/src/test/java/com/springboot/openapi/generator/SpringBootOpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.openapi.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenApiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
